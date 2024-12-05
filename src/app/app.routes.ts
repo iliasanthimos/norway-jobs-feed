23756fba6a8fdcc5b41de450869a646fd9625bf2
iliasanthimos <96637213+iliasanthimos@users.noreply.github.com>
2024-12-05 17:20:17 +0200

    Update app.routes.ts
    -Update routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./features/home/home.component').then(m => m.HomeComponent),
+  },
+  {
+    path: 'favorites',
+    loadComponent: () =>
+      import('./features/favorites/favorites.component').then(m => m.FavoritesComponent),
+  },
+  {
+    path: 'job/:id',
+    loadComponent: () =>
+      import('./features/job-listing-details/job-listing-details.component').then(
+        m => m.JobListingDetailsComponent
+      ),
+  },
+  { path: '**', redirectTo: '' },
+];
