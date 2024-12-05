297fb6244b76c7b64e8360c958b45573f55a371c
iliasanthimos <96637213+iliasanthimos@users.noreply.github.com>
2024-12-05 16:39:19 +0200

    Update Header to use fontawesome
    -Update Header to use fontawesome

--- src/app/shared/ui/header/header.component.ts
+++ src/app/shared/ui/header/header.component.ts
@@ -1,19 +1,34 @@
 import { CommonModule } from '@angular/common';
 import { Component, HostListener, inject } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { NavigationEnd, Router, RouterLink } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faBars, faSun, faMoon, faTimes } from '@fortawesome/free-solid-svg-icons';
 
 import { ThemeService } from '../../../core/services';
 
 @Component({
   selector: 'aa-header',
-  imports: [RouterLink, CommonModule],
+  imports: [RouterLink, CommonModule, FontAwesomeModule],
   templateUrl: './header.component.html',
   standalone: true,
 })
 export class HeaderComponent {
+  private router = inject(Router);
   protected themeService = inject(ThemeService);
 
+  faBars = faBars;
+  faTimes = faTimes;
+  faSun = faSun;
+  faMoon = faMoon;
   isMenuOpen = false;
+
+  constructor() {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.isMenuOpen = false;
+      }
+    });
+  }
 
   @HostListener('window:resize', ['$event'])
   onResize(event: Event): void {
