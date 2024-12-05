649b5b894062b55b3d6dcfb51fa96023117fc6bf
iliasanthimos <96637213+iliasanthimos@users.noreply.github.com>
2024-12-05 16:37:50 +0200

    Add Job Listing Skeleton
    -Add Job Listing Skeleton

---
+++ src/app/shared/ui/job-listing-details-skeleton/job-listing-details-skeleton.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'aa-job-listing-details-skeleton',
+  imports: [],
+  templateUrl: './job-listing-details-skeleton.component.html',
+  standalone: true,
+})
+export class JobListingDetailsSkeletonComponent {}
